4d2bcb8e77dbcfebc92b8968e229aad497079d6e
parveen232 <parveen297m210@gmail.com>
2022-09-06 08:24:38 +0530

    winningScore added

--- scoreKeeper/app.js
+++ scoreKeeper/app.js
@@ -4,8 +4,18 @@ const p1Display = document.querySelector('#p1Display');
 const p2Display = document.querySelector('#p2Display');
 
 let p1Score = 0;
+let p2Score = 0;
+let winningScore = 5;
 
-p1Button.addEventListener('click', function(){
-    p1Score += 1;
-    p1Display.textContent = p1Score;
+p1Button.addEventListener('click', function () {
+    if (p1Score !== winningScore) {
+        p1Score += 1;
+        p1Display.textContent = p1Score;
+    }
+})
+p2Button.addEventListener('click', function () {
+    if (p2Score !== winningScore) {
+        p2Score += 1;
+        p2Display.textContent = p2Score;
+    }
 })
