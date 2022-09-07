8f3486eb87eb8203e0c06df8e78a21d7352f8b85
parveen232 <parveen297m210@gmail.com>
2022-09-07 08:23:37 +0530

    refactoring

--- scoreKeeper/app.js
+++ scoreKeeper/app.js
@@ -1,41 +1,42 @@
-const p1Button = document.querySelector('#p1Button');
-const p2Button = document.querySelector('#p2Button');
+// refactoring
+// group together everything that has to do with p1 into an object and for p2 also
+const p1 = {
+    score: 0,
+    button: document.querySelector('#p1Button'),
+    display: document.querySelector('#p1Display')
+}
+const p2 = {
+    score: 0,
+    button: document.querySelector('#p2Button'),
+    display: document.querySelector('#p2Display')
+}
+
 const resetButton = document.querySelector('#reset');
-const p1Display = document.querySelector('#p1Display');
-const p2Display = document.querySelector('#p2Display');
 const winningScoreSelect = document.querySelector('#playto');
 
-let p1Score = 0;
-let p2Score = 0;
 let winningScore = 3;
 let isGameOver = false;
 
-p1Button.addEventListener('click', function () {
+function updateScores(player1, player2) {
     if (!isGameOver) {
-        p1Score += 1;
-        if (p1Score === winningScore) {
+        player1.score += 1;
+        if (player1.score === winningScore) {
             isGameOver = true;
-            p1Display.classList.add('winner');   //has-text-sucess
-            p2Display.classList.add('loser');    //has-text-danger  bulma-helpers-color
-            p1Button.disabled = true; //bulma
-            p2Button.disabled = true; //bulma
+            player1.display.classList.add('winner');   //has-text-sucess
+            player2.display.classList.add('loser');    //has-text-danger  bulma-helpers-color
+            player1.button.disabled = true; //bulma
+            player2.button.disabled = true; //bulma
         }
-        p1Display.textContent = p1Score;
+        player1.display.textContent = player1.score;
     }
+}
+
+p1.button.addEventListener('click', function () {
+    updateScores(p1, p2);
 })
 
-p2Button.addEventListener('click', function () {
-    if (!isGameOver) {
-        p2Score += 1;
-        if (p2Score === winningScore) {
-            isGameOver = true;
-            p2Display.classList.add('winner');
-            p1Display.classList.add('loser');
-            p1Button.disabled = true; //bulma
-            p2Button.disabled = true; //bulma
-        }
-        p2Display.textContent = p2Score;
-    }
+p2.button.addEventListener('click', function () {
+    updateScores(p2, p1);
 })
 
 winningScoreSelect.addEventListener('change', function () {
@@ -43,17 +44,28 @@ winningScoreSelect.addEventListener('change', function () {
     reset();
 })
 
-resetButton.addEventListener('click', reset)
+resetButton.addEventListener('click', reset);
 
+// if player are more than 2 a loop is better option
 function reset() {
     isGameOver = false;
-    p1Score = 0;
-    p2Score = 0;
-    p1Display.textContent = 0;
-    p2Display.textContent = 0;
-    p1Display.classList.remove('winner', 'loser');
-    p2Display.classList.remove('winner', 'loser');
-    p1Button.disabled = false; //bulma
-    p2Button.disabled = false; //bulma
+    for (let p of [p1, p2]) {
+        p.score = 0;
+        p.display.textContent = 0;
+        p.display.classList.remove('winner', 'loser');
+        p.button.disabled = false; //bulma
+    }
 }
+
+// function reset() {
+//     isGameOver = false;
+//     p1.score = 0;
+//     p2.score = 0;
+//     p1.display.textContent = 0;
+//     p2.display.textContent = 0;
+//     p1.display.classList.remove('winner', 'loser');
+//     p2.display.classList.remove('winner', 'loser');
+//     p1.button.disabled = false; //bulma
+//     p2.button.disabled = false; //bulma
+// }
 
